ec9dde9a8ac47e238ef690f2e9c74b0c537c8b90
Raphael Souza <raphael@devmagic.com.br>
2018-10-17 20:26:40 -0300

    fix(refactor): new version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,99 +9,42 @@ import { CandidateService } from '../app/services/candidates.service';
 export class AppComponent {
 
   loading: boolean = false;
-  counter: any;
-  candidates: any[];
-  overview: any;
-  totalCountedVotesPercent : any;
-  totalCanceledVotes: any;
-  overviewPercent: any;
+  // counter: any;
+  // candidates: any[];
+  // overview: any;
+  // totalCountedVotesPercent : any;
+  // totalCanceledVotes: any;
+
   reload: any;
 
   constructor(private candidateService : CandidateService) { }
 
+  title = 'apura-brasil';
+
   ngOnInit() {
-    this.getCandidates();
+    this.getCandidatesFirstRound();
+    this.getCandidatesSecondRound();
   }
 
    refreshResults(){
      this.reload = setInterval(() => {
        console.log('refreshResults');
-       this.getCandidates();
+      //  this.getCandidates();
      }, 15000);
    }
 
-// todo display count refresh on screen
-  // refreshResults(){
-  //   let counter = 30;
-  //   setInterval(() => {
-  //     counter--
-  //     console.log(counter);
-  //     if(counter == 0){
-  //       counter = 31;
-  //       this.getCandidates();
-  //     }
-  //   }, 1000);
-  // }
+  // return all data from first round ellection
+  getCandidatesFirstRound(){
 
-  // return all data from this election
-  getCandidates(){
-    this.loading = true;
-    clearInterval(this.reload);
-    this.candidateService.get().then((response: any) => {
-      this.loading = false;
-      this.overview = response;
-      this.candidates = response.cand;
-
-      this.candidates.sort(function (a, b) {
-        return parseInt(a.seq) - parseInt(b.seq);
-      });
-
-      this.totalCanceledVotes = this.getTotalCanceledVotes(response);
-      this.overviewPercent = this.getOverviewPercent(response);
-      this.refreshResults();
-      console.log(response);
-    }, (err) => {
-      this.refreshResults();
-      console.log('err', err);
-    })
   }
 
-  getCandidatesGlobo(){
-    this.candidateService.getGlobo().then((response : any) => {
-      console.log(response);
-    })
+  // return all data from second round ellection
+  getCandidatesSecondRound() {
+
   }
 
-  //   total eleitores [e]
-  //   Votos apurados   [ea]
-  //   Votos Restantes  [ena]
-  //   Votos validos    [vv]
-  //   Votos cancelados [vb + vn + a]
-
-  //return the percentage of totals
-  getOverviewPercent(total){
-    let dataPercent;
-    return dataPercent = {
-      totalCounted: ((parseInt(total.vv) * 100) / parseInt(total.vv)).toFixed(0),
-      totalPending: ((parseInt(total.ena) * 100) / parseInt(total.vv)).toFixed(0),
-      totalValid: ((parseInt(total.vv) * 100) / parseInt(total.vv)).toFixed(0),
-      totalCanceled: (((parseInt(total.vb) + parseInt(total.vn) + parseInt(total.a))*100) / parseInt(total.vv)).toFixed(0),
-    }
-  }
-
-  // return the total sum of (votos em branco + votos nulos + abstenencia)
-  getTotalCanceledVotes(total){
-    //   Votos cancelados [vb + vn + a]
-    return parseInt(total.vb)+parseInt(total.vn)+parseInt(total.a);
-  }
-
-  getCandidateVotesPercent(votes){
-    let vv = this.overview.vv;
-    return ((parseInt(votes) * 100) / parseInt(vv)).toFixed(0);
-  } 
 
 
-  title = 'apura-brasil';
 
 
 }
--- src/app/services/candidates.service.ts
+++ src/app/services/candidates.service.ts
@@ -5,19 +5,17 @@ import { Http } from '@angular/http';
 
 export class CandidateService {
 
-  public static url: string = 'http://divulga.tse.jus.br/2018/divulgacao/oficial/295/dadosdivweb/br/br-c0001-e000295-w.js'
+  public static ApiTse: string = 'http://divulga.tse.jus.br/2018/divulgacao/oficial/295/dadosdivweb/br/br-c0001-e000295-w.js'
+  public static ApiApura: string = 'http://apurabrasil.com.br:3000/executivo.json';
 
   constructor(public http: Http) { }
 
-  get(){
-    return this.http.get(CandidateService.url).toPromise().then((res) => res.json());
-    // uncomment for local and moc data
-    // return this.http.get('../../assets/data.json').toPromise().then((res) => res.json());
+  getCandidatesFirstRound(){
+    return this.http.get('../../assets/data.json').toPromise().then((res) => res.json());
   }
 
-  getGlobo(){
-    return this.http.get('http://apurabrasil.com.br:3000/executivo.json').toPromise().then((res) => res.json());
-    // return this.http.get('http://s.glbimg.com/jo/el/2018/apuracao/1-turno/br/executivo.json').toPromise().then((res) => res.json());
+  getCandidatesSecondRound(){
+    return this.http.get(CandidateService.ApiApura).toPromise().then((res) => res.json());
   }
 
   
